1819862d474f2d1187be9b4213d63b3a949e3138
j-awn <j-awn@planetmail.net>
2021-03-17 10:01:38 -0400

    python 3.9 only

--- setup.py
+++ setup.py
@@ -19,16 +19,14 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="jawn",
     author_email='j-awn@planetmail.net',
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="improved chemi-compiler-compiler for goonstation",
     entry_points={
